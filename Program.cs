27b1a5acd400a87515604c3f19de2e55e0dc002a
EfremovaLolita <klass1020@yandex.ru>
2023-04-09 17:19:25 +0300

    сохранение строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿string[] n = {"Hello","2","world","123"};
 string[] result =new string[2];
+int a = 0;
+for(int i=0; i < 4; i++){
+    if (n[i].Length < 4){
+        result[a] = n[i];
+        a = a + 1;
+    }
+}
